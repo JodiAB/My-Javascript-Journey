b412c1734d400f0fe4b0d02d6266de4004bae456
JodiAB <jodiabrahams9@gmail.com>
2023-11-08 11:01:22 +0200

    variables completed

---
+++ script.js
@@ -0,0 +1,55 @@
+const language = "Javascript";
+
+let score = 10;
+
+var length = 10;
+var width = 5;
+
+let area = length * width;
+
+console.log("The area of the rectangle is", area );
+
+
+var num1 = 10;
+var num2 = 4;
+
+let area2 = num1%num2;
+
+console.log("the remainder is", area2)
+
+var yourint = 5;
+var oursdecimal = 1.2;
+const thatstring = 'awesome string';
+var yoarray = [5, 10, 15, 20];
+var brahobject =
+{
+    "name": "John",
+    "age": 30,
+    "isStudent": false,
+    "hobbies": ["reading", "coding"]
+  }
+
+console.log(thatstring);
+console.log(oursdecimal );
+console.log(yoarray);
+console.log(yourint);
+console.log(brahobject);
+
+console.log(typeof thatstring);
+console.log(typeof oursdecimal );
+console.log(typeof yoarray);
+console.log(typeof yourint);
+console.log(typeof brahobject);
+
+var myarray = [1,2,3,4];
+var comment = "it is a object";
+
+console.log(myarray);
+
+console.log(typeof myarray);
+
+console.log(null);
+let obj;
+
+console.log(obj);
+
